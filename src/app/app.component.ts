80d77ab06500c87642642b151c0249ca78dcb740
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-09-04 16:58:13 -0300

    Using an Angular Expression

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Alurapic';
   description = 'Lion';
   url = 'https://upload.wikimedia.org/wikipedia/commons/5/5a/Sultan_the_Barbary_Lion.jpg';
 }
