acc8198332bccd574435b5507bb8575f80b039d2
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-09-04 16:57:26 -0300

    Applying one-way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  description = 'Lion';
+  url = 'https://upload.wikimedia.org/wikipedia/commons/5/5a/Sultan_the_Barbary_Lion.jpg';
 }
