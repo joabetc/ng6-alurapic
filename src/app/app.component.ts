ea214bf1630e573fd2e4403391866572f36f4122
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-09-06 13:24:18 -0300

    Using ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Alurapic';
+
+  photos = [
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/5/5a/Sultan_the_Barbary_Lion.jpg',
+      description: 'Lion'
+    },
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/5/5a/Sultan_the_Barbary_Lion.jpg',
+      description: 'Lion'
+    }
+  ];
 }
