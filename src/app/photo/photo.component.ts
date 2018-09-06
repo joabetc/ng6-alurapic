eab72644513e5950ea79bbf938ce1f91d55c8a45
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-09-06 12:58:28 -0300

    Using Inbound properties

--- src/app/photo/photo.component.ts
+++ src/app/photo/photo.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-photo',
   templateUrl: 'photo.component.html'
 })
 export class PhotoComponent {
-  description = 'Lion';
-  url = 'https://upload.wikimedia.org/wikipedia/commons/5/5a/Sultan_the_Barbary_Lion.jpg';
+
+  @Input() description = '';
+  @Input() url = '';
 }
